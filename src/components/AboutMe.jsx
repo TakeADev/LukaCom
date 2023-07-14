8ce09fdab9f38a63156ee1dacfa68530df28c14d
TakeADev <chanceconwaydev@gmail.com>
2023-07-14 13:59:20 -0400

    Add About Me section.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -16,7 +16,26 @@ function AboutMe() {
         <AboutMeImage src={LukaCloseUp} alt='Luka Closeup' />
       </AboutMeColumn>
       <AboutMeColumn>
-        <AboutMeText>Hey there! I'm Luka. I'm a dog.</AboutMeText>
+        <AboutMeText>
+          <h2>About Me</h2>
+          <p>
+            Hey there! I'm Luka. I'm a dog. People always say, "Hey, Luka! Why
+            do you look so weird?" and to that I say, "my parents hated me!" I'm
+            a dachshund, corgi, and jack russel mix. That's why I looks so
+            derpy!
+          </p>
+          <p>
+            I have a ton of fun interests and hobbies. Let me list some for you:
+          </p>
+          <ul>
+            <li>Sleeping</li>
+            <li>Eating</li>
+            <li>Running after toys and not bringing them back.</li>
+            <li>Digging in blankets and pretending I'm getting somewhere.</li>
+            <li>Licking the heads of bald people.</li>
+            <li>Snuggling.</li>
+          </ul>
+        </AboutMeText>
       </AboutMeColumn>
     </AboutMeContainer>
   )
